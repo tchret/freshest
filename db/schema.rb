156fc198354a9470884130b71a0e65560d7543ac
Amelie Beurrier <abeurrier@gmail.com>
2015-05-08 15:31:33 +0200

    generated Influencer and Post models -- added associations, validations -- raked db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508124530) do
+ActiveRecord::Schema.define(version: 20150508132813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "influencers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "twitter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "influencer_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "posts", ["influencer_id"], name: "index_posts_on_influencer_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +50,5 @@ ActiveRecord::Schema.define(version: 20150508124530) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "posts", "influencers"
 end
