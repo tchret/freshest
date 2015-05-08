816e5ace17162c752f24e39c6d6e383e7aef47dc
Thomas Chretien <thomas@chretien.co>
2015-05-08 17:22:59 +0200

    remove invisible span

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
       twitter_path = influencer.twitter_id[1..-1]
       html_file = open("https://twitter.com/#{twitter_path}")
       html_doc = Nokogiri::HTML(html_file)
-      freshest_post = html_doc.search('.ProfileTweet-text').first.html
+      freshest_post = html_doc.search('.ProfileTweet-text').first
+      freshest_post.css('span.invisible').remove
       @posts << Post.find_or_create_by(content: freshest_post, influencer: influencer)
     end
   end
