2a8346d06e736c06cfa765e462504fc584b01016
Amelie Beurrier <abeurrier@gmail.com>
2015-05-08 16:39:41 +0200

    coded controllers, added posts#index template and added a seed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
+require 'open-uri'
+require 'nokogiri'
+
 class PostsController < ApplicationController
+  def index
+    @posts = []
+    Influencer.all.each do |influencer|
+      twitter_path = influencer.twitter_id[1..-1]
+      html_file = open("https://twitter.com/#{twitter_path}")
+      html_doc = Nokogiri::HTML(html_file)
+      freshest_post = html_doc.search('.ProfileTweet-text').first.text
+      @posts << Post.find_or_create_by(content: freshest_post, influencer: influencer)
+    end
+  end
 end
