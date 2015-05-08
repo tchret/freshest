a67f563fb2b6c444c1fbe67c5e7838b38ff48f5b
Eschults <edward.schults@gmail.com>
2015-05-08 16:58:08 +0200

    replaced .text by .html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
       twitter_path = influencer.twitter_id[1..-1]
       html_file = open("https://twitter.com/#{twitter_path}")
       html_doc = Nokogiri::HTML(html_file)
-      freshest_post = html_doc.search('.ProfileTweet-text').first.text
+      freshest_post = html_doc.search('.ProfileTweet-text').first.html
       @posts << Post.find_or_create_by(content: freshest_post, influencer: influencer)
     end
   end
