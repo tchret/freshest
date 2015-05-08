c783cc9ff2a7eee0875269660f5ecde0e92522c5
Amelie Beurrier <abeurrier@gmail.com>
2015-05-08 17:22:17 +0200

    nokogiri posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,12 @@ require 'nokogiri'
 
 class PostsController < ApplicationController
   def index
-    @posts = []
-    Influencer.all.each do |influencer|
+    @posts = Influencer.all.reduce(Array.new) do |array, influencer|
       twitter_path = influencer.twitter_id[1..-1]
       html_file = open("https://twitter.com/#{twitter_path}")
       html_doc = Nokogiri::HTML(html_file)
-      freshest_post = html_doc.search('.ProfileTweet-text').first.html
-      @posts << Post.find_or_create_by(content: freshest_post, influencer: influencer)
+      freshest_post = html_doc.search('.ProfileTweet-text').first
+      array << Post.find_or_create_by(content: freshest_post, influencer: influencer)
     end
   end
 end
