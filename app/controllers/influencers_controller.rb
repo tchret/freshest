9b5ff4917c9bb608757fd928a5bf36ad7e99a01c
Thomas Chretien <thomas@chretien.co>
2015-05-09 17:43:22 +0200

    create ok

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 
 class InfluencersController < ApplicationController
   before_action :authenticate_user!, only: :create
+  before_action :initialize_service
 
   def index
     @influencers = Influencer.all
@@ -11,24 +12,22 @@ class InfluencersController < ApplicationController
   def show
     influencer = Influencer.where(twitter_id: params[:id])[0]
     twitter_service = TwitterService.new
-    last_tweet = twitter_service.get_last_tweet(influencer.twitter_id)
     twitter_user = twitter_service.client.user(influencer.twitter_id)
     @post = Post.find_or_create_by(
-      content: last_tweet.full_text,
+      content: twitter_service.get_last_tweet(influencer.twitter_id).full_text,
       influencer: influencer,
-      image_url: twitter_user.profile_image_url.to_s.gsub('_normal', '')
+      image_url: twitter_user.profile_image_url.to_s.gsub('normal', '400x400')
     )
   end
 
   def create
     @influencer = Influencer.new(influencer_params)
-    parsing = Parsing.new(influencer)
-    twitter_profile = parsing.get_twitter_profile
-    @influencer.name = twitter_profile.search('.ProfileHeaderCard-nameLink').text
+    last_tweet = @twitter_service.get_last_tweet(influencer.twitter_id)
     if @influencer.save
-      @post = Post.new
-      @post.influencer = @influencer
-      @post.content = twitter_profile.search('.ProfileTweet-text').first
+      @post = Post.new(
+        influencer: @influencer,
+        content: last_tweet.full_text
+      )
       @post.save
       redirect_to 'posts/index'
     else
@@ -40,5 +39,9 @@ class InfluencersController < ApplicationController
 
   def influencer_params
     params.require(:influencer).permit(:twitter_id)
+  end
+
+  def initialize_service
+    @twitter_service = TwitterService.new
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,10 @@ require 'nokogiri'
 class PostsController < ApplicationController
   before_action :initialize_service, only: :index
   def index
-    begin
-      @tweets_with_user = Influencer.all[0..1].reduce(Array.new) do |array, influencer|
-        last_tweet = @twitter_service.get_last_tweet(influencer.twitter_id)
-        twitter_user = @twitter_service.client.user(influencer.twitter_id)
-        array << {user: twitter_user, tweet: last_tweet, influencer: influencer}
-      end
-    rescue
-      raise
+    @tweets_with_user = Influencer.all[0..1].reduce(Array.new) do |array, influencer|
+      last_tweet = @twitter_service.get_last_tweet(influencer.twitter_id)
+      twitter_user = @twitter_service.client.user(influencer.twitter_id)
+      array << {user: twitter_user, tweet: last_tweet, influencer: influencer}
     end
   end
 
