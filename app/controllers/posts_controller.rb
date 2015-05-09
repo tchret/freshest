745d3589ea6e7504199df1b90cd8368efa932669
Thomas Chretien <thomas@chretien.co>
2015-05-09 19:48:18 +0200

    remove nokogiri

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
-require 'open-uri'
-require 'nokogiri'
-
 class PostsController < ApplicationController
   before_action :initialize_service, only: :index
   def index
-    @tweets_with_user = Influencer.all[0..1].reduce(Array.new) do |array, influencer|
+
+    @tweets_with_user = get_last_from_list.reduce(Array.new) do |array, influencer|
       last_tweet = @twitter_service.get_last_tweet(influencer.twitter_id)
       twitter_user = @twitter_service.client.user(influencer.twitter_id)
       array << {user: twitter_user, tweet: last_tweet, influencer: influencer}
