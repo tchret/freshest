9655f71cc3494b822b1805757351bb2d0bc2f9fb
Eschults <edward.schults@gmail.com>
2015-05-09 14:32:26 +0200

    added image_url and refacto parsing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   def index
     @posts = Influencer.all[0..2].reduce(Array.new) do |array, influencer|
       parsing = Parsing.new(influencer)
-      post = Post.find_or_create_by(content: parsing.freshest_post, influencer: influencer)
+      post = Post.find_or_create_by(content: parsing.freshest_post, influencer: influencer, image_url: parsing.get_twitter_image)
       array << post
     end
   end
