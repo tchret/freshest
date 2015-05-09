3efb52ae2bfc26b14de6c3ba48d08d5921207d06
Thomas Chretien <thomas@chretien.co>
2015-05-09 14:13:02 +0200

    fix little error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 
 class PostsController < ApplicationController
   def index
-    @posts = Influencer[0..2].reduce(Array.new) do |array, influencer|
+    @posts = Influencer.all[0..2].reduce(Array.new) do |array, influencer|
       parsing = Parsing.new(influencer)
       post = Post.find_or_create_by(content: parsing.freshest_post, influencer: influencer)
       array << post
