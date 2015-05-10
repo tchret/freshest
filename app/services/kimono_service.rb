142c13c636bad777752b372acfcbe14b91b1c03c
Thomas Chretien <thomas@chretien.co>
2015-05-10 18:27:20 +0200

    new object tweet + created_at supported

--- app/services/kimono_service.rb
+++ app/services/kimono_service.rb
@@ -2,17 +2,46 @@ require 'rest-client'
 
 class KimonoService
   attr_reader :response
+
   def initialize
+    RestClient.post "https://www.kimonolabs.com/kimonoapis/8b6fnvc4/startcrawl", apikey: "zj0UIbRDN9o2vsuGGQpVztNcFI9Udzch"
     @response = JSON.parse(RestClient.get 'https://www.kimonolabs.com/api/8b6fnvc4?apikey=zj0UIbRDN9o2vsuGGQpVztNcFI9Udzch')
   end
 
-  def list_tweets
+  def raw_list
     @response["results"]["collection1"]
   end
 
-  def list_fresh_tweets
-    list_tweets.select do |tweet|
-      tweet["retweet"]["href"].blank?
+  def fresh_tweets
+    clean_list(tweet_list)
+  end
+
+
+  private
+
+
+  def tweet_list
+    @response["results"]["collection1"].reduce(Array.new) do |array, tweet_from_kimono|
+      tweet = Tweet.new(tweet_from_kimono).generate
+      array << tweet
+    end
+  end
+
+
+  def clean_list(list)
+    fresh_list = remove_retweet(list)
+    remove_doubloon(fresh_list)
+  end
+
+  def remove_doubloon(list)
+    list.uniq do |tweet|
+      tweet[:user_id]
+    end
+  end
+
+  def remove_retweet(list)
+    list.select do |tweet|
+      tweet[:retweet]
     end
   end
 
