a4148a0596f2b832801f3047323d7af1de90e778
Thomas Chretien <thomas@chretien.co>
2015-05-10 13:34:28 +0200

    push

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,10 @@
 class PostsController < ApplicationController
   before_action :initialize_service, only: :index
   def index
-    if need_update?
     @twitter_service.get_last_from_list('freshst-dev').reduce(@last_tweets = Array.new) do |array, tweet|
-      influencer = Influencer.find_by(twitter_id: "#{tweet.user.user_name}")
+      influencer = Influencer.find_or_create_by(twitter_id: "#{tweet.user.user_name}", name: "#{tweet.user.name}")
       influencer.update_avatar(tweet.user.profile_image_url.to_s.gsub('normal', '400x400'))
       @last_tweets << {tweet: tweet, influencer: influencer}
-
-    else
-      # use back end
-    end
     end
   end
 
