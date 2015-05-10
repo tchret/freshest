6e34ae665d040f651a91d2c256b69b11bf284e6c
Thomas Chretien <thomas@chretien.co>
2015-05-10 18:27:20 +0200

    new object tweet + created_at supported

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,7 @@
 class PostsController < ApplicationController
   before_action :initialize_kimono_service, only: :index
   def index
-
-
-    @kimono_service.list_fresh_tweets.reduce(@last_tweets = Array.new) do |array, tweet|
-      tweet = {
-        content: tweet["content"]["text"],
-        href: tweet["link"].class == Array ? tweet["link"].first["href"] : false,
-        user_image: tweet["avatar"]["src"].gsub('bigger', '400x400'),
-        user_id: tweet["username"]["text"],
-        user_name: tweet["full_name"]["text"]
-      }
-      @last_tweets << tweet
-    end
+    @fresh_tweet = @kimono_service.fresh_tweets
   end
 
   private
