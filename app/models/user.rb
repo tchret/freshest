df8c6a1501c5b0a6137d83e741a34deb68b51ff5
Eschults <edward.schults@gmail.com>
2015-05-14 14:19:34 +0200

    fixed twitter signin; changed root to posts#index

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
   devise :omniauthable, :omniauth_providers => [:twitter]
+
+  def self.find_for_twitter_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.picture = auth.info.image
+      user.token = auth.credentials.token
+      user.secret = auth.credentials.secret
+    end
+  end
+
+  def email_required?
+    false
+  end
 end
