c8d9b0844a32a43ddda70a861401ff44a5d6d165
Thomas Chretien <thomas@chretien.co>
2015-05-16 21:34:17 +0200

    modal without link

---
+++ app/assets/javascripts/components/connect_modal.js.jsx
@@ -0,0 +1,58 @@
+var ConnectModal = React.createClass({
+  getInitialState: function() {
+    return {
+      opened: false
+    };
+  },
+
+
+  render: function(){
+
+    var modalClasses = React.addons.classSet({
+      "modal-connect": true,
+      "active": this.state.opened
+    })
+
+    var modalContentClasses = React.addons.classSet({
+      "modl-content": true,
+      "pullUp": this.state.opened
+    })
+
+    return(
+      <div className="modal-component">
+        <a className='button' onClick={this.handleClick}>
+          <span>
+            Suggest an influencer
+          </span>
+        </a>
+        <div className={modalClasses} onClick={this.handleClick}>
+          <div className={modalContentClasses}>
+            <ul className='influencers-avatar list-inline text-center'>
+              { this.props.influencer_avatars.map(function(influencer_avatar) {
+                return (
+                  <li>
+                    <img src={influencer_avatar} className='avatar' />
+                  </li>
+                  )
+              }) }
+            </ul>
+            <h3 className='text-center'>
+              Join the community today by adding your favorite influencer
+            </h3>
+            <div className='text-center'>
+              <a className='button button-twitter'>
+                <i className='fa fa-twitter'></i> Sign up with Twitter
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  },
+
+  handleClick: function(){
+    this.setState({
+      opened: this.state.opened? false : true
+    })
+  }
+})
