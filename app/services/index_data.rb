4dd7374840fc764ff5b0b25320729179855fa5da
Eschults <edward.schults@gmail.com>
2015-05-23 18:09:16 +0200

    push to tchret

--- app/services/index_data.rb
+++ app/services/index_data.rb
@@ -4,7 +4,6 @@ class IndexData < KimonoService
   attr_reader :response
 
   def initialize
-    RestClient.post "https://www.kimonolabs.com/kimonoapis/8b6fnvc4/startcrawl", apikey: "zj0UIbRDN9o2vsuGGQpVztNcFI9Udzch"
     @response = JSON.parse(RestClient.get 'https://www.kimonolabs.com/api/8b6fnvc4?apikey=zj0UIbRDN9o2vsuGGQpVztNcFI9Udzch')
   end
 
