dc5af8eff818b46b1bf53195b07ceb30f3ee3363
Amelie Beurrier <abeurrier@gmail.com>
2015-05-23 18:09:16 +0200

    push to tchret

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   respond_to :json, only: :get_meta
   def index
-    @influencers = Influencer.all.reject { |influencer| influencer.last_post_at.nil? }.sort_by(&:last_post_at).reverse.first 5
+    @influencers = Influencer.all.reject { |influencer| influencer.last_post_at.nil? }.sort_by(&:last_post_at).reverse
     # @first_post_meta = MetaInspector.new(@last_posts.first[:href])
   end
 
