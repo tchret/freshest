33fd5b84d3e59ea9ed59f226ba8d1fe02708a2ce
Thomas Chretien <thomas@chretien.co>
2015-05-23 12:50:00 +0200

    react ok

--- app/assets/javascripts/components/sidebar.js.jsx
+++ app/assets/javascripts/components/sidebar.js.jsx
@@ -4,15 +4,10 @@ var Sidebar = React.createClass({
       <div className='sidebar'>
         {this.props.influencers.map(function (influencer) {
             return (
-              <InfluencerListElement href={'/meta?href=' + influencer.href} parentSidebar={this} influencer={influencer} />
+              <InfluencerListElement href={'/meta?href=' + influencer.href} parentComponent={this} influencer={influencer} />
             )
-        })}
+        }, this.props.layout)}
       </div>
     )
-  },
-
-  handlePostChange: function(post) {
-    // this.props.layout.displayPost(post)
-    console.log(post)
   }
 })
