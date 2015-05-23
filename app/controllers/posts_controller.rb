d4f50f7351f30ed42632a3898ed2e3b8f00501b1
Thomas Chretien <thomas@chretien.co>
2015-05-23 15:54:37 +0200

    ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   respond_to :json, only: :get_meta
   def index
-    @influencers = Influencer.all
+    @influencers = Influencer.all.sort_by(&:last_post_time_ago).first 5
     # @first_post_meta = MetaInspector.new(@last_posts.first[:href])
   end
 
