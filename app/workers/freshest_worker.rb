ed629b5969d861065ad75764519c1510a05b172a
Amelie Beurrier <edward.schults@gmail.com>
2015-06-03 17:27:58 +0200

    completed seed

--- app/workers/freshest_worker.rb
+++ app/workers/freshest_worker.rb
@@ -20,7 +20,11 @@ class FreshestWorker
         influencer.title = page.title.split(" | ").first
         influencer.description = page.description
         influencer.article_picture = page.meta_tags["name"]["twitter:image:src"] ? page.meta_tags["name"]["twitter:image:src"].first : page.images.best
-        influencer.content = page.meta_tags["name"]["content"] ? page.meta_tags["name"]["content"].first : (page.parsed.search(".#{influencer.content_class}").first.nil? ? page.description : page.parsed.search(".#{influencer.content_class}").to_s)
+        if inflencer.content_class.nil?
+          influencer.content = page.meta_tags["name"]["content"] ? page.meta_tags["name"]["content"].first : influencer.description
+        else
+          influencer.content = page.meta_tags["name"]["content"] ? page.meta_tags["name"]["content"].first : (page.parsed.search(".#{influencer.content_class}").first.nil? ? page.description : page.parsed.search(".#{influencer.content_class}").to_s)
+        end
         if influencer.save
           logger.info "#{influencer.name.upcase} WAS UPDATED"
         else
