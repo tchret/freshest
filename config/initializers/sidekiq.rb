906635d985394c3b5dd5a7be06016d79fb80bc47
Amelie Beurrier <edward.schults@gmail.com>
2015-06-03 22:51:34 +0200

    added sidekiq config #2

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.example.com:6379/12' }
+  config.redis = { url: 'http://redis.io:6379/12' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis.example.com:6379/12' }
+  config.redis = { url: 'http://redis.io:6379/12' }
 end
