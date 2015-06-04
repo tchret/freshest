a15a5c571779248fa60799866b67eff28c08f413
Amelie Beurrier <edward.schults@gmail.com>
2015-06-04 18:50:17 +0200

    created rake task to run worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'http://redis.io:6379/12' }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'http://redis.io:6379/12' }
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'http://redis.io:6379/12' }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'http://redis.io:6379/12' }
+# end
