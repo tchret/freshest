284ee6e8a92f6c45be52d7ff019242bfb7c2ff49
Amelie Beurrier <edward.schults@gmail.com>
2015-06-05 12:37:36 +0200

    attempting to debug worker running in prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'http://redis.io:6379/12' }
-# end
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://frshst.herokuapp:6379/0' }
+end
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: 'http://redis.io:6379/12' }
-# end
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://frshst.herokuapp:6379/0' }
+end
