080208e0203a2fa9cd8b1b8f9a7804b9b20b37b2
Amelie Beurrier <edward.schults@gmail.com>
2015-06-05 15:28:42 +0200

    running tests on sidekiq & redis #2

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://frshst.herokuapp:6379/0' }
-# end
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'pub-redis-17529.eu-west-1-1.1.ec2.garantiadata.com:17529' }
+end
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: 'redis://frshst.herokuapp:6379/0' }
-# end
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'pub-redis-17529.eu-west-1-1.1.ec2.garantiadata.com:17529' }
+end
