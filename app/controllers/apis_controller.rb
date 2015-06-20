418ad52721a10384c26438986ced5c32c61719b1
Thomas Chretien <thomas@chretien.co>
2015-06-20 03:10:30 +0200

    moving to master

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -4,9 +4,7 @@ class ApisController < ApplicationController
   def reading_list
     user = User.where(twitter_id: params[:id])[0]
     if user
-      @influencers = user.all_follows.map do |follow|
-        Influencer.find(follow.followable_id)
-      end
+      @influencers = user.all_follows.map {|subscription| Influencer.find(subscription.followable_id)}.reject { |influencer| influencer.last_post_at.nil? }.sort_by(&:last_post_at).reverse.first(5)
     else
       @influencers = false
     end
