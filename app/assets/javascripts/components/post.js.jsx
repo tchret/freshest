95c1a4399a2ffc0cee8ebfd9a8bab6476d1a306f
Amelie Beurrier <edward.schults@gmail.com>
2015-06-24 08:05:16 +0200

    worked on responsiveness

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -11,13 +11,16 @@ var Post = React.createClass({
       <div className="post-container">
         <div className="container">
             <div className={"post post-" + source.id } onClick={this.displayPost}>
-              <div className='col-xs-12 col-sm-10'>
+              <div className='col-xs-12'>
                 <ul className="list-unstyled list-inline source-infos">
                   <li>
                     {source.name}
                   </li>
                   <li className="font-light color-light small">
                     {timeSince(new Date(source.last_post_at))} ago
+                  </li>
+                  <li className="pull-right post-avatar p20 kill-pr">
+                    <img src={source.avatar_url} className="img total-width shadowed border-radius-2" />
                   </li>
                 </ul>
                 <div className="post-content">
@@ -29,8 +32,7 @@ var Post = React.createClass({
                   </p>
                 </div>
               </div>
-              <div className="col-sm-2 hidden-xs post-avatar p20 kill-pr">
-                <img src={source.avatar_url} className="img total-width shadowed border-radius-2" />
+              <div className="col-xs-2 post-avatar p20 kill-pr">
               </div>
               <div className='clear' />
             </div>
