72e28e7c52f03fa31c8e522b8f00316a58ad6451
Thomas Chretien <thomas@chretien.co>
2015-06-28 23:36:00 +0200

    quick fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,10 @@ module ApplicationHelper
     avge = 0
     sum = 0
     Source.all.each do |source|
-      time = ((Time.now.to_i - source.last_post_at.to_time.to_i) / 60)
-      sum += time
+      if source.last_post_at
+        time = ((Time.now.to_i - source.last_post_at.to_time.to_i) / 60)
+        sum += time
+      end
     end
     if sum / Source.all.count > 2880
       avge = 2880
