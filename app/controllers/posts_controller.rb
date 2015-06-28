676d32ea8965669f14c3afcbde06474500940f78
Thomas Chretien <thomas@chretien.co>
2015-06-28 23:51:12 +0200

    quick fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
       @sources = current_user.all_follows.map {|subscription| Source.find(subscription.followable_id)}.reject { |source| source.last_post_at.nil? }.sort_by(&:last_post_at).reverse
       @sources = Kaminari.paginate_array(@sources).page(params[:page]).per(5)
     else
-      @sources = Source.all.order(last_post_at: :desc).page(params[:page]).per 5
+      @sources = Source.all.order(last_post_at: :desc).reject { |source| source.last_post_at.nil? }
+      @sources = Kaminari.paginate_array(@sources).page(params[:n]).per(5)
     end
   end
 
@@ -14,7 +15,8 @@ class PostsController < ApplicationController
       sources = current_user.all_follows.map {|subscription| Source.find(subscription.followable_id)}.reject { |source| source.last_post_at.nil? }.sort_by(&:last_post_at).reverse
       @sources = Kaminari.paginate_array(sources).page(params[:n]).per(5)
     else
-      @sources = Source.all.order(last_post_at: :desc).page(params[:n]).per 5
+      @sources = Source.all.order(last_post_at: :desc).reject { |source| source.last_post_at.nil? }
+      @sources = Kaminari.paginate_array(@sources).page(params[:n]).per(5)
     end
   end
 end
