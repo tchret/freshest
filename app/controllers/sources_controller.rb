77e633833d883ffeb5ab3d856e30bab52ccc91e8
Amelie Beurrier <edward.schults@gmail.com>
2015-07-01 12:32:00 +0200

    ok condition fixed

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -5,7 +5,7 @@ class SourcesController < ApplicationController
 
   def show
     if user_signed_in?
-      @sources = current_user.all_follows.map {|subscription| Source.find(subscription.followable_id)}.reject { |source| source.last_post_at.nil?  }.sort_by(&:last_post_at).reverse.first(16)
+      @sources = current_user.all_follows.map {|subscription| Source.find(subscription.followable_id)}.reject { |source| source.last_post_at.nil? || ((Time.now.to_i - source.last_post_at.to_time.to_i) / 60) > current_user.crisp_average }.sort_by(&:last_post_at).reverse.first(16)
       @sources = Kaminari.paginate_array(@sources).page(params[:page]).per(5)
       @source =  Source.where(twitter_id: params[:id])[0]
 
