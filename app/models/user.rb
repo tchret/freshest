5193488c815c1fd4700dc10bd176bb6702ef72ca
Edward Schults <edward.schults@gmail.com>
2015-12-14 13:49:51 +0100

    added include CrsipHelper in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,8 @@ class User < ActiveRecord::Base
 
   has_many :sources
 
+  include CrispHelper
+
   def self.find_for_twitter_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
