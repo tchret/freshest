b7f66ef16605c7b503b54e09a5e6ad3e4811c245
Edward Schults <edward.schults@gmail.com>
2015-12-22 17:10:44 +0100

    added sleep in avatars_worker and rejected posts where description empty

--- app/workers/avatars_worker.rb
+++ app/workers/avatars_worker.rb
@@ -11,20 +11,26 @@ class AvatarsWorker
     logger.info "*" * 40
     ts = TwitterService.new.client
     users = User.all
+    i = 0
     users.each do |user|
-      unless user.id == 169
-        begin
-          unless user.picture == ts.user(user.twitter_id).profile_image_url.to_s
-          logger.info "updating avatar for #{user.twitter_id.upcase}...."
-            user.picture = ts.user(user.twitter_id).profile_image_url.to_s
-            if user.save
-              logger.info "#{user.twitter_id.upcase}'s avatar updated!"
-            else
-              logger.info "#{user.twitter_id.upcase}'s avatar NOT updated :("
+      if i % 15 == 0
+        sleep(900)
+      else
+        unless user.id == 169
+          begin
+            unless user.picture == ts.user(user.twitter_id).profile_image_url.to_s
+            logger.info "updating avatar for #{user.twitter_id.upcase}...."
+              user.picture = ts.user(user.twitter_id).profile_image_url.to_s
+              if user.save
+                logger.info "#{user.twitter_id.upcase}'s avatar updated!"
+              else
+                logger.info "#{user.twitter_id.upcase}'s avatar NOT updated :("
+              end
             end
+          rescue Twitter::Error::NotFound => error
+            logger.info error
           end
-        rescue Twitter::Error::NotFound => error
-          logger.info error
+          i += 1
         end
       end
     end
